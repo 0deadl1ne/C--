993fc4df6305602ce34e86077841ecc91121e357
0deadl1ne <46044294+0deadl1ne@users.noreply.github.com>
2019-11-05 14:05:18 +0200

    Козлова

---
+++ laba 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input four-digit number: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            int n, m, b, c;
+            n = a % 10;//4
+            b = a % 100 / 10;//3
+            m = a % 1000 / 100;//2
+            c = a / 1000;//1
+            if (n == c&& b == m)
+            {
+                Console.WriteLine("True");
+            } else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ laba1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+           
+
+            int b, c;
+            b = a / 3600;
+            c = a / 60 % 60;
+            a = a % 60;
+            Console.WriteLine($"{b}hours");
+            Console.WriteLine($"{c}minutes");
+            Console.WriteLine($"{a}seconds");
+
+            Console.ReadLine();
+
+        }
+    }
+}
